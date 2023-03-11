a02a53e5639f682f54411e9ce5bb6ba2ddcbf92a
Telematica <linuxeron@gmail.com>
2023-03-10 20:53:48 -0600

    Raising Code Coverage %. Modified Request Header Lang

--- src/constants/youtube.constants.js
+++ src/constants/youtube.constants.js
@@ -15,14 +15,14 @@ module.exports = {
     "sec-fetch-mode": "navigate",
     "sec-fetch-user": "?1",
     "sec-fetch-dest": "document",
-    "accept-language": "en-US,en;q=0.9",
+    "accept-language": "es-MX,es;q=0.9",
     cookie: process.env.COOKIE,
   },
   YOUTUBE_LIVE_HEADERS: {
     authority: "www.youtube.com",
     accept:
       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "en-US,en;q=0.9",
+    "accept-language": "es-MX,es;q=0.9",
     cookie:
       "VISITOR_INFO1_LIVE=T1SF4zda-Sw; LOGIN_INFO=AFmmF2swRAIgQXORz8nJ9cguaG2qu6py36elqFw-sTzAoW_5CfBVf64CIBri5r71aIMFYjs7DpFv7d_et04aYqKbzuNSmUZBRikX:QUQ3MjNmekVyMkhGZHVnUWVkWWtlcVlrLVVWdzNpQVBXSGdQdHFieU4zZlVlRHZpczl3d0thbk1pSDYzQUJQRThsNUt5c1Y0eUJQdkFiU3FyR2pvYzA0YkZEd0NWNHdiOHJRYlJVU2Y2YV9NbDNiTEZyX3hHaDBhR2N5LXlYM2p1c29nTE1WRzR3YUNZRDZYYlpITXVUVzNFR0c4aUV2MzRn; NID=511=e8eoRdiqDztPAcrEVNABQBna3mh-2RgKnbBTCwJilHeDfwYt39HKngs91EH2TdLnIIhLvhykrr9NfjKyWI_6fi2Wt_SIhwMC6Nt12Tjb5gL6qIRC-AzUuCEFJa7yoz-wRHr0adMS_F7Dnhe_JIOJvqwKutbjKL30qzfOCb03gd0; HSID=ACbp2x0R8Y-FlW6aX; SSID=AjmVi9pWIqNRc9eNf; APISID=zzpwkQNNrHxxQOO2/A8mXX1qRq7PiPGsx9; SAPISID=gcPg8jl06aSZgo6j/AR-CXtf3OtkFyGNcC; __Secure-1PAPISID=gcPg8jl06aSZgo6j/AR-CXtf3OtkFyGNcC; __Secure-3PAPISID=gcPg8jl06aSZgo6j/AR-CXtf3OtkFyGNcC; DEVICE_INFO=ChxOekU0TnpZek9UZ3lOemd6TURrMk16STFNZz09ENfA/p0GGNfA/p0G; SID=TggmYnJakhgrrN8wSHcWxCoDu2onwa8LUT7CV__Eg3HaZ0YUFH0RIwFLGxao7o4B-JEmhw.; __Secure-1PSID=TggmYnJakhgrrN8wSHcWxCoDu2onwa8LUT7CV__Eg3HaZ0YUzZlIAx_Im4HRGri2hRjD7g.; __Secure-3PSID=TggmYnJakhgrrN8wSHcWxCoDu2onwa8LUT7CV__Eg3HaZ0YU7lFU4EqsosyiO4Dxa2kNMQ.; PREF=f6=40000080&tz=America.Mexico_City&f5=30000&volume=35&f7=150&f4=4000000; YSC=zvsk7oPlMx4; SIDCC=AFvIBn-vWNIt1lMoLt_bDFJPRxDHVpXxHkYqIyG0dSZOa1wvcS_A5YnTyy73N7BfakPHAWwtXnic; __Secure-1PSIDCC=AFvIBn88ADoyc91qT6IiG3vmISae8wVBz9tSRIueyc_tofXYcsagEsavvP7e2CjsU4tIKJuuIr9_; __Secure-3PSIDCC=AFvIBn9IwUKWyXbJAwH0C8Otoe2h7URrsHhCr8xeIkb6gZpmrPSxfEza1hf2L5_EDXB5pAprXUED",
     "sec-ch-ua":
