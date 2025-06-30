aa4e4b3664ae42bd26142926e9bbe67d4877243e
Telematica <linuxeron@gmail.com>
2025-06-30 01:11:49 -0600

    coverage(): Added more UTs.

--- tests/bots/notification.bot.spec.js
+++ tests/bots/notification.bot.spec.js
@@ -8,6 +8,7 @@ const Perf = require("../../src/utils/performance.utils");
 const {
   openOrCreateAndWriteErrorLogFile,
 } = require("../../src/utils/file-system.utils");
+const tiktokUserScrapper = require("../../src/scrappers/tiktok/tiktok-user.scrapper");
 
 jest.mock("../../src/db/connect.db", () => {
   return {
@@ -19,7 +20,26 @@ jest.mock("../../src/db/connect.db", () => {
         findAll: () => [],
       },
       tiktok_user: {
-        findAll: () => [],
+        findAll: () => [
+          { unique_id: "test", id: "test_id", disable_notification: 0 },
+        ],
+      },
+      tiktok_live: {
+        create: () => jest.fn(),
+        findOne: () => [
+          {
+            room_id: "test",
+            tiktok_user_id: "test_id",
+            live: true,
+            title: "",
+            thumbnail: "",
+            view_count: 0,
+            live_since: 0,
+          },
+        ],
+      },
+      tiktok_log_entry: {
+        create: () => jest.fn(),
       },
       live: {
         findOne: jest.fn(() => Promise.resolve(null)),
@@ -28,9 +48,10 @@ jest.mock("../../src/db/connect.db", () => {
       log_entry: {
         create: jest.fn(() => Promise.resolve({})),
       },
-      sequelize: {
-        close: jest.fn(() => Promise.resolve({})),
-      },
+    },
+    sequelize: {
+      transaction: jest.fn((callback) => callback()),
+      close: jest.fn(() => Promise.resolve({})),
     },
   };
 });
@@ -51,12 +72,12 @@ jest.mock("../../src/utils/performance.utils");
 
 jest.mock("../../src/utils/file-system.utils");
 
+jest.mock("../../src/scrappers/tiktok/tiktok-user.scrapper");
+
 describe("NotificationBot Action", () => {
   beforeAll(() => {
     // jest.replaceProperty(process, "argv", ["--use_strict"]);
-    jest.resetAllMocks();
-  });
-  it("should execute notifyChannels() function actions", async () => {
+    // jest.resetAllMocks();
     jest.mocked(Perf).mockImplementation(() => {
       return {
         begin: jest.fn().mockReturnValue({
@@ -64,10 +85,66 @@ describe("NotificationBot Action", () => {
         }),
       };
     });
+  });
+
+  it("should execute notifyChannels() and notify when a Tiktok or YouTube Channel is live", async () => {
+    jest.mocked(tiktokUserScrapper).mockImplementation(
+      /**
+       * The CallAgain method calls the provided function twice
+       * @param {string} uniqueId function to call twice
+       * @return {any} The result of the function call
+       */
+      (uniqueId) => {
+        return Promise.resolve({
+          live: true,
+          roomId: "test_room_id",
+          title: "test_title",
+          thumbnail: "test_thumbnail",
+          viewCount: 100,
+          liveSince: "2021-01-01T00:00:00.000Z",
+          uniqueId,
+        });
+      }
+    );
+    global.console.log = jest.fn();
     await notifyChannels();
     expect(connect).toHaveBeenCalled();
     expect(showDebugInfo).toHaveBeenCalled();
     expect(Perf).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith(
+      "Esta transmisión ya fue notificada: test_room_id - test: test_id"
+    );
+  });
+
+  it("should execute notifyChannels() and log in the console when a Tiktok or YouTube Channel is not live", async () => {
+    jest.clearAllMocks();
+    jest.mocked(tiktokUserScrapper).mockImplementation(
+      /**
+       * The CallAgain method calls the provided function twice
+       * @param {string} uniqueId function to call twice
+       * @return {any} The result of the function call
+       */
+      (uniqueId) => {
+        return Promise.resolve({
+          live: false,
+          roomId: "test_room_id",
+          title: "test_title",
+          thumbnail: "test_thumbnail",
+          viewCount: 100,
+          liveSince: "2021-01-01T00:00:00.000Z",
+          uniqueId,
+        });
+      }
+    );
+    jest.mocked(connect).mockImplementation(() => Promise.resolve());
+    global.console.log = jest.fn();
+    await notifyChannels();
+    expect(connect).toHaveBeenCalled();
+    expect(showDebugInfo).toHaveBeenCalled();
+    expect(Perf).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith(
+      "El Canal no está en vivo: test: test_id."
+    );
   });
 
   it("should handle notifyChannels() occurring Exceptions", async () => {
